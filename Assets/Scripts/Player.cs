572e3e0bf0a9ebaa3b5f720d08004fe1eea09a26
José Striedinger <jstriedinger2090@gmail.com>
2013-11-13 15:30:12 -0500

    initial

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+    public float jumpSpeed;
+    private Vector3 v3_moveDirection = Vector3.zero;
+    private CharacterController controller;
+	private float disToGround;
+	public bool Gravitronned = false;
+	
+	void Start()
+	{
+		disToGround = collider.bounds.extents.y;
+	}
+ 
+    void Awake ()
+    {
+		   
+	}
+ 
+    void Update ()
+    {
+		if(Input.GetKey(KeyCode.RightArrow))
+			transform.Translate(transform.right * speed*Time.deltaTime);
+		else if(Input.GetKey(KeyCode.LeftArrow))
+			transform.Translate(transform.right * -speed*Time.deltaTime);
+		if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+    		rigidbody.velocity = new Vector3(rigidbody.velocity.x, jumpSpeed, rigidbody.velocity.z);   
+    }
+	
+	
+	public bool IsGrounded () {
+  		return Physics.Raycast(transform.position, -Vector3.up, disToGround + 0.1f);
+	}
+	
+	
+	public void changeGravity()
+	{
+		Debug.Log("gravity was: "+Gravitronned);
+		if(Gravitronned)
+			Physics.gravity *= 1;
+		
+		Physics.gravity *= -1;
+     	Gravitronned = true;
+	
+		Debug.Log("gravity now is: "+Gravitronned);
+	}
+
+}
---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transition : MonoBehaviour {
+	
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+			StartCoroutine(ChangeGravity(other.gameObject));
+	}
+	
+	IEnumerator ChangeGravity(GameObject player)
+	{
+		yield return new WaitForSeconds(0.4f);
+		player.GetComponent<Player>().changeGravity();
+	}
+}
